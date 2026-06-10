30e99e741fa99f7131fe706a07b85c898e871285
Luka <ltabliahsvili@gmail.com>
2026-06-10 15:08:24 -0400

    phase 1: select DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log([1, 2, 3])
+const appTitle = document.getElementById("app-title");
+const movieCount = document.getElementById("movie-count");
+const movieForm = document.getElementById("movie-form");
+const titleInput = document.getElementById("title-input");
+const genreInput = document.getElementById("genre-input");
+const movieList = document.getElementById("movie-list");
+const clearWatchBtn = document.getElementById("clear-watched-btn");
+
+const filterBtns = [...document.querySelectorAll(".filter-btn")];
+
+
+console.log(appTitle);
+console.log(movieCount);
+console.log(movieForm)
+console.log(movieForm)
+console.log(titleInput)
+console.log(genreInput)
+console.log(movieList)
+console.log(clearWatchBtn)
+console.log(filterBtns)
