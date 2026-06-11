48618c6c023a1e3b6c7ae1a7d67d350521178d87
Luka <ltabliahsvili@gmail.com>
2026-06-10 21:23:06 -0400

    phase 4: createMovieCard function, append on submit

--- script.js
+++ script.js
@@ -16,6 +16,46 @@ titleInput.value                  // → whatever you just typed
 // getAttribute("value") → gets the value from the HTML attribute, if it exists
 // .value → gets the current live value inside the input, including what the user typed
 
+function createMovieCard(title, genre) {
+    // 1. Create the outer <li>
+    const card = document.createElement("li");
+    card.classList.add("movie-card");
+    card.setAttribute("data-genre", genre);
+
+    // 2. Create the info section
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("movie-info");
+
+    const titleSpan = document.createElement("span");
+    titleSpan.classList.add("movie-title");
+    titleSpan.textContent = title;
+
+    const genreSpan = document.createElement("span");
+    genreSpan.classList.add("movie-genre");
+    genreSpan.textContent = genre || "No genre";
+
+    infoDiv.append(titleSpan, genreSpan)
+
+    // 3. Create the actions section
+    const actionsDiv = document.createElement("div");
+    actionsDiv.classList.add("movie-actions");
+
+    const watchBtn = document.createElement("button");
+    watchBtn.classList.add("watch-btn");
+    watchBtn.textContent = "Mark Watched";
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
+
+    actionsDiv.append(watchBtn, removeBtn)
+
+    // 4. Append the info and actions divs into the <li>
+    card.append(infoDiv, actionsDiv);
+
+    // 5. return the card
+    return card;
+}
 
 movieForm.addEventListener("submit", (e) => {
 
@@ -24,7 +64,10 @@ movieForm.addEventListener("submit", (e) => {
     const title = titleInput.value;
     const genre = genreInput.value;
 
-    console.log(title);
-    console.log(genre);
+    const movieCard = createMovieCard(title, genre);
+    movieList.appendChild(movieCard);
+
     movieForm.reset();
-})
+});
+
+
